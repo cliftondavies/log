ae7fd95123b9f9f318c2ac128e43d70904026deb
Clifton Davies <20408665+cliftondavies@users.noreply.github.com>
2023-09-20 18:19:34 +0000

    test client discovery and balancing end-to-end

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -17,6 +17,7 @@ import (
 	api "github.com/cliftondavies/log/api/v1"
 	"github.com/cliftondavies/log/internal/agent"
 	"github.com/cliftondavies/log/internal/config"
+	"github.com/cliftondavies/log/internal/loadbalance"
 )
 
 func TestAgent(t *testing.T)  {
@@ -82,15 +83,16 @@ func TestAgent(t *testing.T)  {
 		Record: &api.Record{Value: []byte("foo")},
 	})
 	require.NoError(t, err)
+
+	// wait until replication has finished
+	time.Sleep(3 * time.Second)
+
 	consumeResponse, err := leaderClient.Consume(context.Background(), &api.ConsumeRequest{
 		Offset: produceResponse.Offset,
 	})
 	require.NoError(t, err)
 	require.Equal(t, consumeResponse.Record.Value, []byte("foo"))
 
-	// wait until replication has finished
-	time.Sleep(3 * time.Second)
-	
 	followerClient := client(t, agents[1], peerTLSConfig)
 	consumeResponse, err = followerClient.Consume(context.Background(), &api.ConsumeRequest{
 		Offset: produceResponse.Offset,
@@ -113,7 +115,7 @@ func client(t *testing.T, agent *agent.Agent, tlsConfig *tls.Config) api.LogClie
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
 	rpcAddr, err := agent.Config.RPCAddr()
 	require.NoError(t, err)
-	conn, err := grpc.Dial(rpcAddr, opts...)
+	conn, err := grpc.Dial(fmt.Sprintf("%s:///%s", loadbalance.Name, rpcAddr), opts...)
 	require.NoError(t, err)
 	client := api.NewLogClient(conn)
 	return client
